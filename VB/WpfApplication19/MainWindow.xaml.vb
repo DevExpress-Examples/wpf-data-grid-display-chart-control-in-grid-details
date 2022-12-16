11d198f027bded1f23a8e15cc9c0d25446f05708
DevExpressExampleBot <fakeEmail>
2022-12-16 16:33:57 +0400

    Source auto update [skip ci]

--- VB/WpfApplication19/MainWindow.xaml.vb
+++ VB/WpfApplication19/MainWindow.xaml.vb
@@ -1,196 +1,202 @@
-﻿Imports System
 Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 Imports DevExpress.Xpf.Grid
 
 Namespace WpfApplication19
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-'INSTANT VB NOTE: The field employees was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private employees_Conflict As List(Of Employee)
-		Private ReadOnly Property Employees() As List(Of Employee)
-			Get
-				If employees_Conflict Is Nothing Then
-					PopulateEmployees()
-				End If
-				Return employees_Conflict
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-			End Get
-		End Property
-		Private Sub PopulateEmployees()
-			employees_Conflict = New List(Of Employee)()
-			employees_Conflict.Add(New Employee("Bruce", "Cambell", "Sales Manager", "Education includes a BA in psychology from Colorado State University in 1970.  He also completed 'The Art of the Cold Call.'  Bruce is a member of Toastmasters International."))
-			employees_Conflict(0).Orders.Add(New Order("Supplier 1", DateTime.Now, "TV", 20))
-			employees_Conflict(0).Orders.Add(New Order("Supplier 2", DateTime.Now.AddDays(3), "Projector", 15))
-			employees_Conflict(0).Orders.Add(New Order("Supplier 3", DateTime.Now.AddDays(3), "HDMI Cable", 50))
-			employees_Conflict.Add(New Employee("Cindy", "Haneline", "Vice President of Sales", "Cindy received her BTS commercial in 1974 and a Ph.D. in international marketing from the University of Dallas in 1981.  She is fluent in French and Italian and reads German.  She joined the company as a sales representative, was promoted to sales manager in January 1992 and to vice president of sales in March 1993.  Cindy is a member of the Sales Management Roundtable, the Seattle Chamber of Commerce, and the Pacific Rim Importers Association."))
-			employees_Conflict(1).Orders.Add(New Order("Supplier 1", DateTime.Now.AddDays(1), "Blu-Ray Player", 10))
-			employees_Conflict(1).Orders.Add(New Order("Supplier 2", DateTime.Now.AddDays(1), "HDMI Cable", 10))
-			employees_Conflict(1).Orders.Add(New Order("Supplier 3", DateTime.Now.AddDays(1), "Projector", 10))
-			employees_Conflict(1).Orders.Add(New Order("Supplier 4", DateTime.Now.AddDays(1), "Amplifier", 10))
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			gridControl1.ItemsSource = Employees
-			gridControl2.ItemsSource = Employees
-		End Sub
+        Private employeesField As List(Of Employee)
 
-		#Region "#SetupDetails"
-		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			Dim dataControlDetail As New DataControlDetailDescriptor()
-			dataControlDetail.DataControl = SetupDetailGrid()
-			dataControlDetail.ItemsSourcePath = "Orders"
-			dataControlDetail.ContentTemplate = DirectCast(Me.FindResource("ContentDetail"), DataTemplate)
+        Private ReadOnly Property Employees As List(Of Employee)
+            Get
+                If employeesField Is Nothing Then PopulateEmployees()
+                Return employeesField
+            End Get
+        End Property
 
-			Dim contentDetail As New ContentDetailDescriptor()
-			contentDetail.ContentTemplate = DirectCast(Me.FindResource("ContentDetail"), DataTemplate)
-			contentDetail.HeaderContentTemplate = DirectCast(Me.FindResource("ContentDetail"), DataTemplate)
-			contentDetail.HeaderContent = "ContentDetailDescriptor"
+        Private Sub PopulateEmployees()
+            employeesField = New List(Of Employee)()
+            employeesField.Add(New Employee("Bruce", "Cambell", "Sales Manager", "Education includes a BA in psychology from Colorado State University in 1970.  He also completed 'The Art of the Cold Call.'  Bruce is a member of Toastmasters International."))
+            employeesField(0).Orders.Add(New Order("Supplier 1", Date.Now, "TV", 20))
+            employeesField(0).Orders.Add(New Order("Supplier 2", Date.Now.AddDays(3), "Projector", 15))
+            employeesField(0).Orders.Add(New Order("Supplier 3", Date.Now.AddDays(3), "HDMI Cable", 50))
+            employeesField.Add(New Employee("Cindy", "Haneline", "Vice President of Sales", "Cindy received her BTS commercial in 1974 and a Ph.D. in international marketing from the University of Dallas in 1981.  She is fluent in French and Italian and reads German.  She joined the company as a sales representative, was promoted to sales manager in January 1992 and to vice president of sales in March 1993.  Cindy is a member of the Sales Management Roundtable, the Seattle Chamber of Commerce, and the Pacific Rim Importers Association."))
+            employeesField(1).Orders.Add(New Order("Supplier 1", Date.Now.AddDays(1), "Blu-Ray Player", 10))
+            employeesField(1).Orders.Add(New Order("Supplier 2", Date.Now.AddDays(1), "HDMI Cable", 10))
+            employeesField(1).Orders.Add(New Order("Supplier 3", Date.Now.AddDays(1), "Projector", 10))
+            employeesField(1).Orders.Add(New Order("Supplier 4", Date.Now.AddDays(1), "Amplifier", 10))
+        End Sub
 
-			Dim tabDetail As New TabViewDetailDescriptor()
-			tabDetail.DetailDescriptors.Add(contentDetail)
-			tabDetail.DetailDescriptors.Add(dataControlDetail)
+        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.gridControl1.ItemsSource = Employees
+            Me.gridControl2.ItemsSource = Employees
+        End Sub
 
-			gridControl2.DetailDescriptor = tabDetail
-		End Sub
-		#End Region ' #SetupDetails
+'#Region "#SetupDetails"
+        Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Dim dataControlDetail As DataControlDetailDescriptor = New DataControlDetailDescriptor()
+            dataControlDetail.DataControl = SetupDetailGrid()
+            dataControlDetail.ItemsSourcePath = "Orders"
+            dataControlDetail.ContentTemplate = CType(FindResource("ContentDetail"), DataTemplate)
+            Dim contentDetail As ContentDetailDescriptor = New ContentDetailDescriptor()
+            contentDetail.ContentTemplate = CType(FindResource("ContentDetail"), DataTemplate)
+            contentDetail.HeaderContentTemplate = CType(FindResource("ContentDetail"), DataTemplate)
+            contentDetail.HeaderContent = "ContentDetailDescriptor"
+            Dim tabDetail As TabViewDetailDescriptor = New TabViewDetailDescriptor()
+            tabDetail.DetailDescriptors.Add(contentDetail)
+            tabDetail.DetailDescriptors.Add(dataControlDetail)
+            Me.gridControl2.DetailDescriptor = tabDetail
+        End Sub
 
-		#Region "#SetupDetailGrid"
-		Private Function SetupDetailGrid() As GridControl
-			Dim detailGrid As New GridControl()
-			detailGrid.AutoGenerateColumns = AutoGenerateColumnsMode.AddNew
-			detailGrid.View = New TableView()
-			TryCast(detailGrid.View, TableView).DetailHeaderContent = "DataControlDetailDescriptor"
-			TryCast(detailGrid.View, TableView).ShowGroupPanel = False
-			TryCast(detailGrid.View, TableView).AutoWidth = True
-			Return detailGrid
-		End Function
-		#End Region ' #SetupDetailGrid
-	End Class
+'#End Region  ' #SetupDetails
+'#Region "#SetupDetailGrid"
+        Private Function SetupDetailGrid() As GridControl
+            Dim detailGrid As GridControl = New GridControl()
+            detailGrid.AutoGenerateColumns = AutoGenerateColumnsMode.AddNew
+            detailGrid.View = New TableView()
+            TryCast(detailGrid.View, TableView).DetailHeaderContent = "DataControlDetailDescriptor"
+            TryCast(detailGrid.View, TableView).ShowGroupPanel = False
+            TryCast(detailGrid.View, TableView).AutoWidth = True
+            Return detailGrid
+        End Function
+'#End Region  ' #SetupDetailGrid
+    End Class
 
-	Public Class Employee
-'INSTANT VB NOTE: The field firstName was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private firstName_Conflict As String
-'INSTANT VB NOTE: The field lastName was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private lastName_Conflict As String
-'INSTANT VB NOTE: The field title was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private title_Conflict As String
-'INSTANT VB NOTE: The field notes was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private notes_Conflict As String
-'INSTANT VB NOTE: The field orders was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private orders_Conflict As List(Of Order)
-		Public Sub New(ByVal firstName As String, ByVal lastName As String, ByVal title As String, ByVal notes As String)
-			Me.firstName_Conflict = firstName
-			Me.lastName_Conflict = lastName
-			Me.title_Conflict = title
-			Me.notes_Conflict = notes
-			Me.orders_Conflict = New List(Of Order)()
-		End Sub
-		Public Sub New()
-		End Sub
-		Public Property FirstName() As String
-			Get
-				Return firstName_Conflict
-			End Get
-			Set(ByVal value As String)
-				firstName_Conflict = value
-			End Set
-		End Property
-		Public Property LastName() As String
-			Get
-				Return lastName_Conflict
-			End Get
-			Set(ByVal value As String)
-				lastName_Conflict = value
-			End Set
-		End Property
-		Public Property Title() As String
-			Get
-				Return title_Conflict
-			End Get
-			Set(ByVal value As String)
-				title_Conflict = value
-			End Set
-		End Property
-		Public Property Notes() As String
-			Get
-				Return notes_Conflict
-			End Get
-			Set(ByVal value As String)
-				notes_Conflict = value
-			End Set
-		End Property
-		Public ReadOnly Property Orders() As List(Of Order)
-			Get
-				Return orders_Conflict
-			End Get
-		End Property
-	End Class
+    Public Class Employee
 
-	Public Class Order
-'INSTANT VB NOTE: The field supplier was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private supplier_Conflict As String
-'INSTANT VB NOTE: The field date was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private date_Conflict As DateTime
-'INSTANT VB NOTE: The field productName was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private productName_Conflict As String
-'INSTANT VB NOTE: The field quantity was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private quantity_Conflict As Integer
-		Public Sub New(ByVal supplier As String, ByVal [date] As DateTime, ByVal productName As String, ByVal quantity As Integer)
-			Me.supplier_Conflict = supplier
-			Me.date_Conflict = [date]
-			Me.productName_Conflict = productName
-			Me.quantity_Conflict = quantity
-		End Sub
-		Public Property Supplier() As String
-			Get
-				Return supplier_Conflict
-			End Get
-			Set(ByVal value As String)
-				supplier_Conflict = value
-			End Set
-		End Property
-		Public Property [Date]() As DateTime
-			Get
-				Return date_Conflict
-			End Get
-			Set(ByVal value As DateTime)
-				date_Conflict = value
-			End Set
-		End Property
-		Public Property ProductName() As String
-			Get
-				Return productName_Conflict
-			End Get
-			Set(ByVal value As String)
-				productName_Conflict = value
-			End Set
-		End Property
-		Public Property Quantity() As Integer
-			Get
-				Return quantity_Conflict
-			End Get
-			Set(ByVal value As Integer)
-				quantity_Conflict = value
-			End Set
-		End Property
-	End Class
+        Private firstNameField As String
 
+        Private lastNameField As String
+
+        Private titleField As String
+
+        Private notesField As String
+
+        Private ordersField As List(Of Order)
+
+        Public Sub New(ByVal firstName As String, ByVal lastName As String, ByVal title As String, ByVal notes As String)
+            firstNameField = firstName
+            lastNameField = lastName
+            titleField = title
+            notesField = notes
+            ordersField = New List(Of Order)()
+        End Sub
+
+        Public Sub New()
+        End Sub
+
+        Public Property FirstName As String
+            Get
+                Return firstNameField
+            End Get
+
+            Set(ByVal value As String)
+                firstNameField = value
+            End Set
+        End Property
+
+        Public Property LastName As String
+            Get
+                Return lastNameField
+            End Get
+
+            Set(ByVal value As String)
+                lastNameField = value
+            End Set
+        End Property
+
+        Public Property Title As String
+            Get
+                Return titleField
+            End Get
+
+            Set(ByVal value As String)
+                titleField = value
+            End Set
+        End Property
+
+        Public Property Notes As String
+            Get
+                Return notesField
+            End Get
+
+            Set(ByVal value As String)
+                notesField = value
+            End Set
+        End Property
+
+        Public ReadOnly Property Orders As List(Of Order)
+            Get
+                Return ordersField
+            End Get
+        End Property
+    End Class
+
+    Public Class Order
+
+        Private supplierField As String
+
+        Private dateField As Date
+
+        Private productNameField As String
+
+        Private quantityField As Integer
+
+        Public Sub New(ByVal supplier As String, ByVal [date] As Date, ByVal productName As String, ByVal quantity As Integer)
+            supplierField = supplier
+            dateField = [date]
+            productNameField = productName
+            quantityField = quantity
+        End Sub
+
+        Public Property Supplier As String
+            Get
+                Return supplierField
+            End Get
+
+            Set(ByVal value As String)
+                supplierField = value
+            End Set
+        End Property
+
+        Public Property [Date] As Date
+            Get
+                Return dateField
+            End Get
+
+            Set(ByVal value As Date)
+                dateField = value
+            End Set
+        End Property
+
+        Public Property ProductName As String
+            Get
+                Return productNameField
+            End Get
+
+            Set(ByVal value As String)
+                productNameField = value
+            End Set
+        End Property
+
+        Public Property Quantity As Integer
+            Get
+                Return quantityField
+            End Get
+
+            Set(ByVal value As Integer)
+                quantityField = value
+            End Set
+        End Property
+    End Class
 End Namespace
